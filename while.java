317e25e327f0d031422febbd99c9c914a78c94ec
dderking <54045471+dderking@users.noreply.github.com>
2019-10-02 21:40:25 -0300

    Create while.java
    While em Java

---
+++ while.java
@@ -0,0 +1,14 @@
+public class EstruturaRepeticao{
+
+  public static void man (String[] args) {
+  
+    int x = 15;
+    
+    while (x < 18){
+      System.out.println("Você não tem permissão para entrar")
+      x++;
+     }
+     
+    }
+    
+}
